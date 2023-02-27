49c36507ac5e6406aff070acfea5f77a1415d871
miyamoto-tatsuki <i.love.wolf090@gmail.com>
2023-02-27 20:25:55 +0900

    更新

---
+++ Assets/ProjectBase/Scripts/ApiManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ApiManager : MonoBehaviour
+{
+
+    IEnumerator GetRequest()
+    {
+        string url = "";
+
+        using(UnityWebRequest webRequest = UnityWebRequest.Get(url))
+        {
+            yield return webRequest.SendWebRequest();
+
+            if(webRequest.isNetworkError)
+            {
+                Debug.Log("error = " + webRequest.error);
+            }
+            else
+            {
+                Debug.Log("sucess = " + webRequest.downloadHandler.text);
+            }
+        }
+    }
+}
