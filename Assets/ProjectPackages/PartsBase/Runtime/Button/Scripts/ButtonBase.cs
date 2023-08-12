69e3972daea31c36a87405797321532753eba3c4
miyamoto-tatsuki <i.love.wolf090@gmail.com>
2023-08-12 11:40:48 +0900

    名前空間の追加

--- Assets/ProjectPackages/PartsBase/Runtime/Button/Scripts/ButtonBase.cs
+++ Assets/ProjectPackages/PartsBase/Runtime/Button/Scripts/ButtonBase.cs
@@ -5,25 +5,28 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using DG.Tweening;
 
-public class ButtonBase : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler
+namespace TanukiParts
 {
-    [NonSerialized]
-    public System.Action OnClick;
-
-    public void OnPointerClick(PointerEventData eventData)
+    public class ButtonBase : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler
     {
-     }
+        [NonSerialized]
+        public System.Action OnClick;
 
-    public void OnPointerDown(PointerEventData e)
-    {
-        this.transform.DOScale(0.9f, 0.1f);
-    }
+        public void OnPointerClick(PointerEventData eventData)
+        {
+        }
 
-    public void OnPointerUp(PointerEventData e)
-    {
-        this.transform.DOScale(1f, 0.1f);
+        public void OnPointerDown(PointerEventData e)
+        {
+            this.transform.DOScale(0.9f, 0.1f);
+        }
 
-        if (OnClick != null)
-            OnClick();
+        public void OnPointerUp(PointerEventData e)
+        {
+            this.transform.DOScale(1f, 0.1f);
+
+            if (OnClick != null)
+                OnClick();
+        }
     }
 }
